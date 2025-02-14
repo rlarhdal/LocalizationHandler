f803c8add13edba784091ff7d0c417c09a731c2e
SooHyeon Kim <93560053+rlarhdal@users.noreply.github.com>
2025-02-14 10:47:48 +0900

    Update LocalizationHandler.cs

--- LocalizationHandler.cs
+++ LocalizationHandler.cs
@@ -1,11 +1,10 @@
-using System;
 using System.Collections.Generic;
+using DefaultNamespace.UI;
+using DefaultNamespace.UI.Retry;
 using Dialogue;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using Object = System.Object;
 
 namespace Handler
 {
@@ -14,16 +13,19 @@ namespace Handler
         private ProcessHandler _processHandler;
         private RegionHandler _regionHandler;
 
-        private List<TextHolder> _objs;
-        private List<Dictionary<string, object>> _dialogData;
+        public SkillSelectUIHandler SkillSelectUIHandler;
+        public RetryUIHandler RetryUIHandler;
+        public DialogHandler DialogHandler;
 
-        public enum DialogueType
+        public enum LocalizationType
         {
-            NULL,
+            None,
             Intro,
-            Lab,
-            BossEncount,
-            Ending,
+            Title,
+            ProcessUI,
+            RetryInfo,
+            RetryUI,
+            Room
         }
         
         public override void OnAwake(ProcessHandler processHandler)
@@ -31,79 +33,136 @@ namespace Handler
             _processHandler = processHandler;
             
             Debug.Log("LocalizationHandler Awake");
+
+            _regionHandler = _processHandler.RegionHandler;
         }
 
-        public void OnSceneLoaded()
+        public override void OnSceneLoaded()
         {
-            // 초기화 신경 써야하는 것들
-            if (_objs != null) _objs.Clear();
-
-            SettingDialog();
+            SetLocalization();
         }
+
+        public void SetLocalization()
+        {
+            // 이벤트 형식으로 변경 예정
+            DialogHandler.SetData();
+            SkillSelectUIHandler.LocalizationSkillInfo(); // skillinfo
+            SettingDialog(); // 다이얼로그 
+            GetUILocalizationData(); // UI
+            RetryUIHandler.LoadRetryData();
+        }
+        
         public void SettingDialog()
         {
             // 컷씬인지 확인
             if (_processHandler.CurSceneType != SceneHandler.SceneType.CutScene) return;
             
             //리젼에 맞는 폰트 가지고 오기 
-            TMP_FontAsset curFont = GetRegionFont();
+            TMP_FontAsset curFont =
+                _processHandler.TMPFontAssetHandler.GetDialogFontAssetByRegionType(_regionHandler.CurRegionType);
             
             //다이얼로그 오브젝트 배열로 받아오기
-            _objs = FindDialogueObjs();
-
-            //폰트 넣기
-            foreach (var VARIABLE in _objs)
-            {
-                VARIABLE.TextMeshProUGUI.font = curFont;
-            }
+            List<TextHolder> objs = FindDialogueTextHolderObjs();
 
             //csv데이터 오브젝트에 삽입
-            MatchingDialog();
+            List<Dictionary<string, string>> dialogData = GetDatasWithCurrScene();
+            ApplyData(objs, dialogData, curFont);
+        }
+        
+        public void GetUILocalizationData()
+        {
+            //리젼에 맞는 폰트 가지고 오기 
+            TMP_FontAsset curFont =
+                _processHandler.TMPFontAssetHandler.GetUIFontAssetByRegionType(_regionHandler.CurRegionType);
+            
+            //다이얼로그 오브젝트 배열로 받아오기
+            List<TextHolder> objs = FindUITextHolderObjs();
+            
+            //csv데이터 오브젝트에 삽입
+            List<Dictionary<string, string>> localizationUIDatas = ProcessHandler.Instance.CSVHandler.LoadLocalizationDatas(CSVHandler.FilePath.UI, LocalizationType.ProcessUI.ToString());
+            ApplyData(objs, localizationUIDatas, curFont);
         }
 
-        private void MatchingDialog()
+        private void ApplyData(List<TextHolder> objs, List<Dictionary<string, string>> datas, TMP_FontAsset font)
         {
-            _dialogData = GetCurrentScene();
-
-            foreach (var VARIABLE in _objs)
+            foreach (var VARIABLE in objs)
             {
-                for (int j = 0; j < _dialogData.Count; j++)
+                if (datas == null)
                 {
-                    Object curDialogObjName = _dialogData[j][CSVHandler.HeaderType.OBJECT_NAME.ToString()];
+                    Debug.LogWarning("LocalizationHandler에서 List<Dictionary<string, string>> 이 Null");
+                    continue;
+                }
+                
+                for (int i = 0; i < datas.Count; i++)
+                {
+                    if(datas[i].Count != 2) continue;
+                    
+                    string curDialogObjName = datas[i][CSVHandler.HeaderType.OBJECT_NAME.ToString()];
+                    string dialog = datas[i][_regionHandler.CurRegionType.ToString()];
 
-                    if (VARIABLE.gameObject.name == curDialogObjName.ToString())
+                    string identifier = VARIABLE.GetCurrentIdentifier() == null ? VARIABLE.gameObject.name : VARIABLE.GetCurrentIdentifier();
+                    if (identifier == curDialogObjName)
                     {
-                        VARIABLE.TextMeshProUGUI.text =
-                            _dialogData[j][ProcessHandler.Instance.CSVHandler.CurHeader.ToString()].ToString();
+                        VARIABLE.SetText(dialog);
+                        VARIABLE.SetFontAsset(font);
                     }
                 }
             }
         }
 
-        private TMP_FontAsset GetRegionFont()
-        {
-            // 리젼 갖고 있으면 리젼에 맞는 폰트 가져오기
-            return ProcessHandler.Instance.RegionHandler.GetFontAssetByRegionType(ProcessHandler.Instance.RegionHandler.CurRegionType);
-        }
 
-        private List<TextHolder> FindDialogueObjs()
+        private List<TextHolder> FindUITextHolderObjs()
         {
-            return new List<TextHolder>(GameObject.FindObjectsOfType<TextHolder>());
+            List<TextHolder> textHolders = new List<TextHolder>(GameObject.FindObjectsByType<TextHolder>(FindObjectsInactive.Include, FindObjectsSortMode.None));
+            List<TextHolder> uiTextHolders = new List<TextHolder>();
+            
+            foreach (var VARIABLE in textHolders)
+            {
+                if (VARIABLE.HolderType != TextHolder.TextHolderType.Dialogue)
+                {
+                    uiTextHolders.Add(VARIABLE);
+                }
+            }
+
+            return uiTextHolders;
+        }
+        
+        private List<TextHolder> FindDialogueTextHolderObjs()
+        {
+            List<TextHolder> textHolders = new List<TextHolder>(GameObject.FindObjectsByType<TextHolder>(FindObjectsInactive.Include, FindObjectsSortMode.None));
+            List<TextHolder> dialogueTextHolders = new List<TextHolder>();
+            
+            foreach (var VARIABLE in textHolders)
+            {
+                if (VARIABLE.HolderType == TextHolder.TextHolderType.Dialogue)
+                {
+                    dialogueTextHolders.Add(VARIABLE);
+                }
+            }
+
+            return dialogueTextHolders;
         }
 
         //>>임시<< 씬 이름 받아오기
-        private List<Dictionary<string, object>> GetCurrentScene()
+        private List<Dictionary<string, string>> GetDatasWithCurrScene()
         {
             string curScene = SceneManager.GetActiveScene().name;
             
             switch (curScene)
             {
                 case "intro":
-                    return ProcessHandler.Instance.CSVHandler.LoadDialogue(DialogueType.Intro);
+                    return ProcessHandler.Instance.CSVHandler.LoadLocalizationDatas(CSVHandler.FilePath.Dialog, LocalizationType.Intro.ToString());
+                
+                case "TitleScreen":
+                    return ProcessHandler.Instance.CSVHandler.LoadLocalizationDatas(CSVHandler.FilePath.Dialog, LocalizationType.Title.ToString());
+                
+                case "Room":
+                    return ProcessHandler.Instance.CSVHandler.LoadLocalizationDatas(CSVHandler.FilePath.Dialog, LocalizationType.Room.ToString());
                 
                 default:
-                    return ProcessHandler.Instance.CSVHandler.LoadDialogue(DialogueType.Intro);
+                    return ProcessHandler.Instance.CSVHandler.LoadLocalizationDatas(CSVHandler.FilePath.Dialog, LocalizationType.None.ToString());
             }
         }
+
     }
 }
